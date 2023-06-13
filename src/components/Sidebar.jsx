65ce11f47c1d3bdca87affaee3293b5b9f2378b1
luizsanches <luizsanches@luizsanchess-MacBook-Pro.local>
2023-06-13 19:51:32 -0300

    Navbar & Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,13 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const handleCloseSidebar = () => {
+    if(activeMenu && screenSize <= 900){
+      setActiveMenu(false);
+    }
+  }
+
   const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700' +
   ' dark:text-gray-200 dark:hover:text-black m-2';
@@ -21,7 +27,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSidebar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
@@ -29,9 +35,7 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => setActiveMenu(
-                    (prevActiveMenu) => !prevActiveMenu
-                  )}
+                onClick={handleCloseSidebar}
                 className="text-xl rounded-full p-3 hover::bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
